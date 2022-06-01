4b634b3c707ae7a6368748f556e6881b05f257c0
wr0110 <wittyraccoon0110@gmail.com>
2022-06-01 17:40:47 -0400

    Ohio header and nav

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,19 +11,19 @@ export default function Footer({ stateLabel, facebook, twitter }: Props) {
 	return (
 		<footer>
 			<div className="bg-[#4a84b2] py-16">
-				<div className="container flex items-center">
-					<img src="/footer.png" width={100} className="basis-0" />
-					<p className="text-gray-900 text-[1rem] max-w-3xl ml-auto">
+				<div className="container md:flex gap-8 items-center">
+					<img src="/footer.png" width={75} className="basis-0 -mt-4" />
+					<p className="text-gray-900 text-[1rem] max-w-md lg:max-w-3xl mt-4 md:mt-0 md:ml-auto">
 						<strong>This website provides descriptions and maps of <a href="https://ebird.org" className="text-gray-900" target="_blank" rel="noreferrer">eBird</a> Hotspots in {stateLabel}</strong>. In eBird, Hotspots are shared locations where birders may report their bird sightings to eBird. Hotspots provide birders with information about birding locations where birds are being seen.
 					</p>
 				</div>
 			</div>
 			<div className="bg-[#325a79] py-3">
-				<div className="container flex gap-6 justify-end text-2xl">
-					<a href={`https://www.facebook.com/`} className="text-white" target="_blank" rel="noreferrer">
+				<div className="container flex gap-6 justify-end text-xl">
+					<a href={`https://www.facebook.com/${facebook}`} className="text-white" target="_blank" rel="noreferrer">
 						<FacebookIcon />
 					</a>
-					<a href={`https://twitter.com/oos_birds`} className="text-white" target="_blank" rel="noreferrer">
+					<a href={`https://twitter.com/${twitter}`} className="text-white" target="_blank" rel="noreferrer">
 						<TwitterIcon />
 					</a>
 				</div>
