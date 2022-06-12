164d962a1517a5bbfc23064e7df406c10547224f
wr0110 <wittyraccoon0110@gmail.com>
2022-06-12 19:05:34 -0400

    Format about text

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function AboutSection({ text, heading }: Props) {
 	return (
-		<div className="mb-4">
+		<div className="mb-4 formatted">
 			<h3 className="font-bold">{heading}</h3>
 			<div dangerouslySetInnerHTML={{__html: text}} />
 		</div>
