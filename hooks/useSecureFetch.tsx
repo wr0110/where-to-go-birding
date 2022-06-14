ae730e4521da6d073286b45d987ccd9135ce1a7b
wr0110 <wittyraccoon0110@gmail.com>
2022-06-13 21:40:33 -0400

    Authenticate API requests

---
+++ hooks/useSecureFetch.tsx
@@ -0,0 +1,17 @@
+import { auth } from "lib/firebaseAuth";
+	
+export default function useFetch()  {
+	return async (url: string, method: string, data: any) => {
+		const token = await auth.currentUser?.getIdToken();
+		if (!token) return;
+		const response = await fetch(url, {
+			method: method,
+			headers: {
+				Authorization: token,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		});
+		return await response.json(); 
+	}
+}
--- pages/api/hotspot/add.ts
+++ pages/api/hotspot/add.ts
@@ -1,8 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import connect from "lib/mongo";
+import admin from "lib/firebaseAdmin";
 import Hotspot from "models/Hotspot";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const token = req.headers.authorization;
+
+	try {
+		await admin.verifyIdToken(token || "");
+	} catch (error) {
+		res.status(401).json({ error: "Unauthorized" });
+		return;
+	}
+  
 	try {
     await connect();
     const { data } = req.body;
