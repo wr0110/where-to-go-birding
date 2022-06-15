2e41a766d72a43cf864a438d674e221898a7a8fd
wr0110 <wittyraccoon0110@gmail.com>
2022-06-15 19:56:17 -0400

    Open bar chart links in new tab

--- components/EbirdBarcharts.tsx
+++ components/EbirdBarcharts.tsx
@@ -9,15 +9,15 @@ export default function EbirdBarcharts({ region, portal }: Props) {
 		<div className="mb-6 p-2 border-2 border-[#4a84b2] rounded">
 			<h3 className="font-bold text-lg">eBird Bar Charts</h3>
 			<p>
-				<a href={`${base}/barchart?byr=1900&eyr=2060&bmo=1&emo=12&r=${region}`}>Entire Year</a>
+				<a href={`${base}/barchart?byr=1900&eyr=2060&bmo=1&emo=12&r=${region}`} target="_blank" rel="noreferrer">Entire Year</a>
 				&nbsp;–&nbsp;
-				<a href={`${base}/barchart?byr=1900&eyr=2060&bmo=3&emo=5&r=${region}`}>Spring</a>
+				<a href={`${base}/barchart?byr=1900&eyr=2060&bmo=3&emo=5&r=${region}`} target="_blank" rel="noreferrer">Spring</a>
 				&nbsp;–&nbsp;
-				<a href={`${base}/barchart?byr=1900&eyr=2060&bmo=6&emo=7&r=${region}`}>Summer</a>
+				<a href={`${base}/barchart?byr=1900&eyr=2060&bmo=6&emo=7&r=${region}`} target="_blank" rel="noreferrer">Summer</a>
 				&nbsp;–&nbsp;
-				<a href={`${base}/barchart?byr=1900&eyr=2060&bmo=8&emo=11&r=${region}`}>Fall</a>
+				<a href={`${base}/barchart?byr=1900&eyr=2060&bmo=8&emo=11&r=${region}`} target="_blank" rel="noreferrer">Fall</a>
 				&nbsp;–&nbsp;
-				<a href={`${base}/barchart?byr=1900&eyr=2060&bmo=12&emo=2&r=${region}`}>Winter</a>
+				<a href={`${base}/barchart?byr=1900&eyr=2060&bmo=12&emo=2&r=${region}`} target="_blank" rel="noreferrer">Winter</a>
 			</p>
 		</div>
 	)
