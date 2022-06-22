a23a537cfd04846e258a7eb227538e6967815398
wr0110 <wittyraccoon0110@gmail.com>
2022-06-22 18:13:26 -0400

    Auto resize TinyMCE

--- lib/helpers.tsx
+++ lib/helpers.tsx
@@ -12,13 +12,14 @@ export function getTinyConfig(height?: number) {
   return {
     height: height || 200,
     menubar: false,
-    plugins: "link",
+    plugins: "link autoresize",
     toolbar: "bold italic underline | link | cite",
     content_style:
       "body { font-family:Helvetica,Arial,sans-serif; font-size:14px } cite { font-size: 0.75em; font-style: normal; color: #666; }",
     branding: false,
     elementpath: false,
     valid_elements: "p,a[href|rel|target=_blank],strong/b,em/i,u,strike,br,ul,ol,li,cite",
+    autoresize_bottom_margin: 0,
     formats: {
       citation: { inline: "cite" },
     },
