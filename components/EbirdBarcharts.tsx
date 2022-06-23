8924e1458dc3f9ac527d1b0184044c8f044446d9
wr0110 <wittyraccoon0110@gmail.com>
2022-06-23 07:57:20 -0400

    Change bar chart label

--- components/EbirdBarcharts.tsx
+++ components/EbirdBarcharts.tsx
@@ -7,7 +7,7 @@ export default function EbirdBarcharts({ region, portal }: Props) {
   const base = portal ? `https://ebird.org/${portal}` : "https://ebird.org";
   return (
     <section className="mb-6 p-2 border-2 border-[#4a84b2] rounded">
-      <h3 className="font-bold text-lg">eBird Bar Charts</h3>
+      <h3 className="font-bold text-lg">Explore in eBird</h3>
       <p>
         <a href={`${base}/barchart?byr=1900&eyr=2060&bmo=1&emo=12&r=${region}`} target="_blank" rel="noreferrer">
           Entire Year
