512cd79edc17f9b6c5eb25226a9a38c73502e577
wr0110 <wittyraccoon0110@gmail.com>
2022-07-01 18:06:45 -0400

    Modify script to migrate AZ

--- migrate.mjs
+++ migrate.mjs
@@ -3,25 +3,29 @@ import fetch from "node-fetch";
 import mongoose from "mongoose";
 import Hotspot from "./models/Hotspot.mjs";
 import dotenv from "dotenv";
+import Counties from "./data/az-counties.json" assert {type: "json"}; // IMPORTANT: ------------------------------------------------- Update for each state
 dotenv.config();
 
-import Counties from "./data/oh-counties.json" assert {type: "json"};
-import IBA from "./data/oh-iba.json" assert {type: "json"};
+import { links } from "./migrate-az.mjs";
+//const links = ["usaz-apache-county/usaz-canyon-de-chelly-national-monument"];
 
 const URI = process.env.MONGO_URI;
 mongoose.connect(URI);
 
-const county = "ottawa";
 const dryRun = false;
-const nameExceptions = ["Magee Marsh--Entrance Road", "Ottawa NWR--Two Rivers Unit, O'Neal Rd. (restricted access)", "Great Egret Marsh Preserve (TNC)", "Ottawa NWR--Kontz Unit, Bodi and Lemon Roads"];
-const processWithoutLocationId = ["Toussaint River", "Turtle Creek Estuary"];
-const skip = ["winous-point"];
-const state = "ohio";
-const stateCode = "OH";
-const base = "https://birding-in-ohio.com";
+const slice = 10;
+const nameExceptions = ["3 Canyons Blvd grasslands"];
+const skip = [];
+const state = "arizona";
+const stateCode = "AZ";
+const base = `https://ebirdhotspots.com/birding-in-${state}`;
 
 if (dryRun) {
 	console.log("--------------------Dry run--------------------");
+}
+
+const cleanSlug = (value) => {
+	return value.replace(`us${stateCode.toLowerCase()}-`, "");
 }
 
 async function getEbirdHotspot(locationId) {
@@ -32,23 +36,11 @@ async function getEbirdHotspot(locationId) {
   }
 }
 
-const getHotspots = async () => {
-	const request = await fetch(`https://birding-in-ohio.com/${county}-county/`);
-	const html = await request.text();
-	const dom = new JSDOM(html);
-	const doc = dom.window.document;
-	const links = doc.querySelectorAll(".et_pb_column_2 a:not(a[href^='https://ebird.org'])");
-	return Array.from(links).map(link => link.href.replace(/\/$/, ""));
-}
-
-const hotspots = await Hotspot.find({ state: stateCode }, ["slug", "locationId"]);
+const hotspots = await Hotspot.find({ stateCode }, ["slug", "locationId"]);
 console.log(`Fetched ${hotspots.length} hotspots from DB`);
 
-const links = await getHotspots();
-console.log(`Found ${links.length} hotspot links`);
-
 let filteredLinks = links.filter(link => {
-	const slug = link.split("/").pop();
+	const slug = cleanSlug(link.split("/").pop());
 	return !hotspots.find(hotspot => hotspot.slug === slug) && !skip.includes(slug);
 });
 
@@ -58,39 +50,77 @@ if (filteredLinks?.length === 0) {
 	throw new Error("No links found");
 }
 
-const getLocationId = (url) => {
-	const segments = url.split("locID=")[1];
-	return segments.split("&")[0];
-}
+filteredLinks = filteredLinks.slice(0, slice);
 
-const processAbout = (html) => {
+console.log(`Scraping ${filteredLinks.length} links`);
+
+const processAbout = (html, parentName) => {
 	let data = {
+		trash: "",
 		tips: "",
-		birds: "",
-		hikes: "",
 		about: ""
 	}
 	let current = "about";
 	let aboutStarted = false;
+
+	const children = Array.from(html.childNodes);
+
+	if (children?.length && (children[0].nodeName === "STRONG" || children[1].nodeName === "STRONG")) {
+		const slicedChildren = children.slice(1);
+		const strong = children[0].nodeName === "STRONG" ? children[0] : children[1];
+		if (strong?.textContent?.trim() === `About ${parentName}`) {
+			current = "trash";
+		} else if (strong?.textContent?.trim()?.startsWith("About ")) {
+			aboutStarted = true;
+			current = "about";
+		}
+		if (strong?.textContent?.toLowerCase()?.includes("tips for birding")) {
+			current = "tips";
+		}
+
+		let content = "";
+		let done = false;
+
+		slicedChildren.forEach((child) => {
+			if ((child.nodeName === "BR" || child.nodeName === "#text" || child.nodeName === "SPAN" || child.nodeName === "STRONG") && !done) {
+				if (child.nodeName === "BR") {
+					content += "\n";
+				} else if (child.nodeName === "STRONG" && (child.textContent?.trim()?.startsWith("About ") || child.textContent?.trim()?.startsWith("Tips"))) {
+					// do nothing
+				} else if (child.nodeName === "SPAN") {
+					content += `<cite>${child.textContent?.trim()}</cite>`;
+				} else {
+					content += child.textContent?.trim() || "";
+				}
+			} else {
+				done = true;
+			}
+		});
+		
+		content = content.replaceAll('&lt;', "<");
+		content = content.replaceAll('&gt;', ">");
+		content = content.replaceAll('<small>', "");
+		content = content.replaceAll('</small>', "");
+		data[current] += content?.trim() || "";
+	}
+
 	const paragraphs = html.querySelectorAll("p");
 	for (const p of paragraphs) {
 		const spanCite = p.querySelector(`span[style*='xx-small']`);
 		if (spanCite) {
 			spanCite.replaceWith(`<cite>${spanCite.innerHTML}</cite>`);
 		}
+		const iframe = p.querySelector("iframe");
+		if (iframe) {
+			return { tips: data.tips, about: data.about }
+		}
 		const strong = p.querySelector("strong");
 		if (strong?.textContent?.trim()?.startsWith("About ") && aboutStarted === true) {
-			return { tips: data.tips, birds: data.birds, about: data.about, hikes: data.hikes }
+			return { tips: data.tips, about: data.about }
 		}
-		if (strong?.textContent?.toLowerCase()?.includes("birding day hike")) {
-			strong.remove();
-			current = "hikes";
-		}
-		if (strong?.textContent?.toLowerCase()?.includes("birds of interest")) {
-			strong.remove();
-			current = "hikes";
-		}
-		if (strong?.textContent?.trim()?.startsWith("About ")) {
+		if (strong?.textContent?.trim() === `About ${parentName}`.trim()) {
+			current = "trash";
+		} else if (strong?.textContent?.trim()?.startsWith("About ")) {
 			strong.remove();
 			aboutStarted = true;
 			current = "about";
@@ -103,7 +133,7 @@ const processAbout = (html) => {
 		if (content.startsWith("<p><br>")) {
 			content = content.replace("<p><br>", "<p>");
 		}
-		content = content.replaceAll('href="/', `href="/${state}/`);
+		content = content.replaceAll("<p></p>", "");
 		content = content.replaceAll('&lt;', "<");
 		content = content.replaceAll('&gt;', ">");
 		content = content.replaceAll('<small>', "");
@@ -111,12 +141,12 @@ const processAbout = (html) => {
 		data[current] += content?.trim() || "";
 	}
 
-	return { tips: data.tips, birds: data.birds, about: data.about, hikes: data.hikes }
+	return { tips: data.tips, about: data.about }
 }
 
 const processImages = (maps, images, photographer) => {
-	const mapArray = maps ? Array.from(maps).filter(img => !img.src.includes(`-county-map.jpg`) && !img.src.includes(`-county-map-g.jpg`)).map(map => ({
-		smUrl: map.src,
+	const mapArray = maps ? Array.from(maps).map(map => ({
+		smUrl: `https://ebirdhotspots.com${map.src}`,
 		lgUrl: null,
 		by: null,
 		width: parseInt(map.getAttribute("width")) || null,
@@ -126,7 +156,7 @@ const processImages = (maps, images, photographer) => {
 	})) : [];
 
 	const imgArray = images ? Array.from(images).map(img => ({
-		smUrl: img.src,
+		smUrl: `https://ebirdhotspots.com${img.src}`,
 		lgUrl: null,
 		by: photographer,
 		width: img.getAttribute("width"),
@@ -138,33 +168,45 @@ const processImages = (maps, images, photographer) => {
 	return [...mapArray, ...imgArray];
 }
 
+const softCompare = (string1, string2) => {
+	string1 = string1.replace(/[^a-z0-9]/gi, '').toLowerCase().trim();
+	string2 = string2.replace(/[^a-z0-9]/gi, '').toLowerCase().trim();
+	return string1 === string2;
+}
+
 const checkIfNamesMatch = (ebird, h1) => {
 	ebird = ebird.trim();
 	h1 = h1.trim();
 	if (!ebird || !h1) return false;
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll(" State Park", " SP");
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll(" National Wildlife Refuge", " NWR");
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
+	h1 = h1.replaceAll(" National Monument", " NM");
+	if (softCompare(ebird, h1)) return true;
+	h1 = h1.replaceAll(" National Park", " NP");
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll("Road", "Rd.");
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll("Drive", "Dr.");
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll("Avenue", "Ave.");
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll("Boulevard", "Blvd.");
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll("Street", "St.");
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll("Township", "Twp.");
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
+	h1 = h1.replaceAll("Wastewater Treatment Plant", "WTP");
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll("-", "--");
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll("–", "--"); //en dash
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
 	h1 = h1.replaceAll("County", "Co.");
-	if (ebird === h1) return true;
+	if (softCompare(ebird, h1)) return true;
 	const newH1 = `${h1} (view from roadside only)`;
 	if (ebird === newH1) return true;
 	const newH1Again = `${h1} (restricted access)`;
@@ -183,181 +225,116 @@ await Promise.all(filteredLinks.map(async (link) => {
 	const dom = new JSDOM(html);
 	const doc = dom.window.document;
 	const slug = link.split("/").pop();
-	const locLink = doc.querySelector(`a[href*='submit/effort?locID']`);
-	const isGroup = !link.startsWith(`/${county}-county/`);
-	if (isGroup && !locLink) {
-		throw new Error(`No locID found for ${link}`);
+	let address = "";
+	let addressLines = 0;
+	let parentSlug = null;
+	let parentName = null;
+
+	const leftCol = doc.querySelector(".csColumn");
+	if (!leftCol) {
+		throw new Error(`No left column found for ${link}`);
 	}
-	const h1Name = doc.querySelector("h1")?.textContent?.trim()?.replace("National Park", "NP") || null;
-	let locationId = isGroup ? null : getLocationId(locLink.href);
 
-	let name = null;
-	let lat = null;
-	let lng = null;
-	let countyCode = null;
-	let multiCounties = null;
+	const children = Array.from(leftCol.childNodes);
 
-	if (locationId) {
-		const ebirdData = await getEbirdHotspot(locationId);
-		if (!ebirdData) {
-			console.log(`WARNING: No ebird data found for ${slug}`);
-		}
-		if (!checkIfNamesMatch(ebirdData?.name, h1Name)) {
-			console.log(`WARNING: mismatch: ${h1Name} vs ${ebirdData.name}`);
-			if (processWithoutLocationId.includes(h1Name)) {
-				console.log("Setting locationId to null");
-				locationId = null;
+	children.forEach((child) => {
+		if ((child.nodeName === "#text" || child.nodeName === "BR") && addressLines < 3) {
+			if (child.nodeName === "BR") {
+				address += "\n";
 			} else {
-				throw new Error("Couldn't resolve mismatch");
+				address += child.textContent?.trim() || "";
 			}
-		} else {
-			const alreadyExists = await Hotspot.findOne({ locationId });
-			if (alreadyExists) {
-				console.log(`WARNING: Skipping "${slug}" with eBird ID "${locationId}" already exists`);
-				return;
-			}
-			name = ebirdData?.name || null;
-			lat = ebirdData?.latitude || null
-			lng = ebirdData?.longitude || null;
-			countyCode = ebirdData?.subnational2Code || null;
+			addressLines++;
+		}
+	});
+	const firstP = leftCol.querySelector("p:not(:empty)");
+	if (firstP) {
+		const firstPText = firstP.textContent?.trim();
+		if (firstPText.startsWith("Also, see")) {
+			parentSlug = firstP.querySelector("a")?.getAttribute("href")?.replace(/\/$/, "")?.split("/")?.pop() || null;
+			parentName = firstP.querySelector("a")?.textContent?.trim() || null;
 		}
 	}
-	if (!locationId) {
-		if (!locationId) console.log("No locID");
-		name = h1Name;
 
-		const countyImages = doc.querySelectorAll("img[src$='-county-map.jpg']");
-		multiCounties = Array.from(countyImages).map(img => {
-			const slug = img.src.split("/").pop()?.replace("-county-map.jpg", "");
-			if (!slug) return;
-			return Counties.find(it => it.slug === slug)?.ebirdCode;
-		}).filter(item => item);
-	}
-
-	const intro = doc.querySelector(".et_pb_text_inner");
-	const externalLinks = intro.querySelectorAll(`a[href*='http']:not(a[href*='ebird.org']):not(a[href*='birding-in-ohio.com'])`);
+	const externalLinks = leftCol.querySelectorAll(`a[href*='http']:not(a[href*='ebird.org']):not(a[href*='ebirdhotspots.com'])`);
 	const links = Array.from(externalLinks).map(link => ({
 		label: link.textContent?.trim() || null,
 		url: link.href,
 	}));
 
-	const parentLink = intro.querySelector(`a[href^='/${county}-county/']`);
-	const parentSlug = parentLink?.href?.split("/${county}-county/")?.pop()?.replace("/", "");
+	const rightCol = doc.querySelectorAll(".csColumn")[1];
+	const h4 = rightCol.querySelector("h4");
+	if (h4) h4.remove();
 
-	const internalLinks = intro.querySelectorAll(`a[href^='/`);
-	let migrateParentGroupSlug = null;
-	Array.from(internalLinks).forEach(link => {
-		const slug = link.href.replace(/\/$/, "");
-		if (links.find(it => it === slug)) {
-			migrateParentGroupSlug = slug;
+
+	if (!rightCol) {
+		throw new Error(`No right column found for ${link}`);
+	}
+
+	const maps = rightCol.querySelectorAll("img");
+	const images = processImages(maps);
+
+	const { tips, about } = rightCol ? processAbout(rightCol, parentName) : {};
+
+	const locationLink = leftCol.querySelector(`table a[href*='ebird.org']`)?.href;
+	const locationId = locationLink?.split("hotspots=")?.[1]?.split("&")?.[0]?.split(",")?.[0] || null;
+	if (!locationId) {
+		throw new Error(`No location link found for ${link}`);
+	}
+	const h1Name = doc.querySelector("h1")?.textContent?.trim()?.replace("National Park", "NP") || null;
+	let name = null;
+	let lat = null;
+	let lng = null;
+	let countyCode = null;
+
+	const ebirdData = await getEbirdHotspot(locationId);
+	if (!ebirdData) {
+		throw new Error(`No ebird data found for ${slug}`);
+	}
+	if (!checkIfNamesMatch(ebirdData?.name, h1Name)) {
+		throw new Error(`Name mismatch: ${h1Name} vs ${ebirdData.name}`);
+	} else {
+		const alreadyExists = await Hotspot.findOne({ locationId });
+		if (alreadyExists) {
+			console.log(`WARNING: Skipping "${slug}" with eBird ID "${locationId}" already exists`);
+			return;
 		}
-	});
-
-	const ibaLink = intro.querySelector(`a[href*='ohio-important-bird-areas/']`);
-	let ibaSlug = ibaLink?.href?.split("ohio-important-bird-areas/").pop();
-	ibaSlug = ibaSlug?.replace("/", "")?.replace("-important-bird-area", "");
-	ibaSlug = ibaSlug?.replaceAll(".", "");
-	const ibaName = IBA.find(iba => iba.slug === ibaSlug)?.name;
-	if (ibaSlug && !ibaName) console.error(`WARNING: No IBA match: ${ibaSlug}`);
-
-	const strongs = intro.querySelectorAll("strong");
-	for (const strong of strongs) {
-		strong.remove();
-	}
-	const paragraphs = intro.querySelectorAll("p");
-	for (const p of paragraphs) {
-		p.remove();
-	}
-	const anchors = intro.querySelectorAll("a");
-	for (const a of anchors) {
-		a.remove();
-	}
-	const address = intro.textContent.trim();
-
-	const aboutHtmlAttempt1 = doc.querySelector(".et_pb_column_2 .et_pb_text_9 .et_pb_text_inner");
-	const aboutHtmlAttempt2 = doc.querySelector(".et_pb_column_2 .et_pb_text_10 .et_pb_text_inner");
-	const aboutHtmlAttempt3 = doc.querySelector(".et_pb_column_2 .et_pb_text_11 .et_pb_text_inner");
-	const aboutHtml = aboutHtmlAttempt1 || aboutHtmlAttempt2 || aboutHtmlAttempt3;
-	if (!aboutHtml) console.log(`WARNING: No about for ${slug}`);
-	const { tips, birds, about, hikes } = aboutHtml ? processAbout(aboutHtml) : {};
-
-	const maps = doc.querySelectorAll(".et_pb_column_2 img");
-	const galleryImages = doc.querySelectorAll(".et_pb_column_1 .et_pb_gallery_0 img");
-
-	let photographerAttempt = doc.querySelector(".et_pb_column_1 .et_pb_gallery_0 + div")?.textContent?.trim();
-	if (!photographerAttempt?.includes("Photo")) {
-		photographerAttempt = null;
-	}
-	const photographer = photographerAttempt?.replace("Photos by ", "")?.replace("Photo by ", "");
-	let images = processImages(maps, galleryImages, photographer);
-
-	const extraImage1 = doc.querySelector(".et_pb_column_1 .et_pb_image_0");
-	const extraImage1By = doc.querySelector(".et_pb_column_1 .et_pb_image_0 + div")?.textContent?.trim();
-	const extraImage2 = doc.querySelector(".et_pb_column_1 .et_pb_image_1");
-	const extraImage2By = doc.querySelector(".et_pb_column_1 .et_pb_image_1 + div")?.textContent?.trim();
-
-	if (extraImage1) {
-		const image = extraImage1.querySelector("img");
-		if (image) {
-			images.push({
-				smUrl: image.src,
-				lgUrl: null,
-				by: extraImage1By?.includes("Photo") ? extraImage1By?.replace("Photos by ", "")?.replace("Photo by ", "") : null,
-				width: image?.getAttribute("width") || null,
-				height: image?.getAttribute("height") || null,
-				legacy: true,
-				isMap: false,
-			});
-		}
+		name = ebirdData?.name || null;
+		lat = ebirdData?.latitude || null
+		lng = ebirdData?.longitude || null;
+		countyCode = ebirdData?.subnational2Code || null;
 	}
 
-	if (extraImage2) {
-		const image = extraImage1.querySelector("img");
-		if (image) {
-			images.push({
-				smUrl: image.src,
-				lgUrl: null,
-				by: extraImage2By?.includes("Photo") ? extraImage2By?.replace("Photos by ", "")?.replace("Photo by ", "") : null,
-				width: image.getAttribute("width") || null,
-				height: image.getAttribute("height") || null,
-				legacy: true,
-				isMap: false,
-			});
-		}
+	const county = Counties.find(it => it.ebirdCode === countyCode)?.slug;
+	if (!county) {
+		throw new Error(`No county found for ${countyCode}`);
 	}
 
-	const url = locationId ? `/${state}/${county}-county/${slug}` : `/${state}/group/${slug}`;
+	const url = `/${state}/${county}-county/${cleanSlug(slug)}`;
 
 	const data = {
 		name,
 		url,
-		slug,
+		slug: cleanSlug(slug),
 		lat,
 		lng,
 		stateCode,
-		multiCounties,
+		multiCounties: null,
 		countyCode,
-		address: address || "",
-		tips: tips || "",
-		birds: birds || "",
-		about: about || "",
-		hikes: hikes || "",
-		slug,
+		address: address.trim() || "",
+		tips: tips.trim() || "",
+		birds: "",
+		about: about.trim() || "",
+		hikes: "",
 		oldSlug: slug,
 		images: images || [],
 		links: links.length > 0 ? links : [],
 		locationId: locationId || null,
-		iba: ibaName && ibaSlug ? {
-			label: ibaName,
-			value: ibaSlug,
-		} : null,
-		migrateParentSlug: name.includes("--") ? parentSlug || null : null,
-		migrateParentGroupSlug: name.includes("--") ? migrateParentGroupSlug || null : null,
+		iba: null,
+		migrateParentSlug: parentSlug ? cleanSlug(parentSlug) : null,
 		reviewed: false,
 	}
-	if (!dryRun) await Hotspot.create(data);
-	if (!dryRun) console.log(`${counter}. Saved`, name);
-	counter++;
+	console.log(data);
 }));
 
 mongoose.connection.close();
