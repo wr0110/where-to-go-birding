0948fe1095d78de673a5fb8961d9397009a5b704
wr0110 <wittyraccoon0110@gmail.com>
2022-08-08 07:43:08 -0400

    Fix footer logo link

--- components/GeneralFooter.tsx
+++ components/GeneralFooter.tsx
@@ -6,19 +6,16 @@ export default function OhioFooter() {
     <div className="bg-[#4a84b2] py-16 text-white">
       <div className="container grid md:grid-cols-4 gap-8">
         <div className="flex gap-2 flex-col">
-          <a
-            href="https://ohiobirds.org/"
-            className="flex gap-4 px-2 py-2 rounded bg-white items-center"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Logo className="w-12" />
-            <div className="leading-3 pb-0.5">
-              <strong className="text-lg text-gray-700 font-normal">Birding Hotspots</strong>
-              <br />
-              <em className="text-[0.8em] text-[#92ad39] font-medium">Where to Go Birding</em>
-            </div>
-          </a>
+          <Link href="/">
+            <a className="flex gap-4 px-2 py-2 rounded bg-white items-center">
+              <Logo className="w-12" />
+              <div className="leading-3 pb-0.5">
+                <strong className="text-lg text-gray-700 font-normal">Birding Hotspots</strong>
+                <br />
+                <em className="text-[0.8em] text-[#92ad39] font-medium">Where to Go Birding</em>
+              </div>
+            </a>
+          </Link>
           <p>This website is managed by Ken Ostermiller, Adam Jackson, and other volunteers.</p>
         </div>
         <div />
