50761b0c1b0c6f7fda214702b9ff45939a441aae
wr0110 <wittyraccoon0110@gmail.com>
2022-08-19 14:01:58 -0400

    Update TopHotspots.tsx

--- components/TopHotspots.tsx
+++ components/TopHotspots.tsx
@@ -15,7 +15,7 @@ export default function TopHotspots({ region, label }: Props) {
   React.useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`/api/hotspot/by-region?region=${region}&limit=8`);
+        const response = await fetch(`/api/hotspot/by-region?region=${region}&limit=10`);
         const json = await response.json();
         setResults(json?.results || []);
       } catch (error) {}
@@ -29,11 +29,11 @@ export default function TopHotspots({ region, label }: Props) {
 
   return (
     <>
-      <div className="mt-12 grid xs:grid-cols-2 md:grid-cols-4 gap-6">
+      <div className="mt-12 grid xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         <HotspotGrid hotspots={results} loading={false} showFullName />
       </div>
       <Link href={`/explore?mode=region&region=${region}&label=${label || region}`}>
-        <a className="bg-[#4a84b2] hover:bg-[#325a79] text-white font-bold py-1.5 text-sm px-4 rounded-full w-[140px] mx-auto block mt-8 text-center">
+        <a className="bg-[#4a84b2] hover:bg-[#325a79] text-white font-bold py-1.5 text-sm px-4 rounded-full w-[140px] mx-auto block mt-4 text-center">
           View More <ArrowNarrowRightIcon className="inline-block w-4 h-4 ml-2" />
         </a>
       </Link>
